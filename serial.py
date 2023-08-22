f7348ff3f31f68a841048d3417b789b0f3b59236
Grace Bethany Hoober <grace.hoober66@gmail.com>
2023-08-22 15:20:27 -0700

    second commit

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,20 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+def __init__(self, start=100):
+    """Create a serial number generator that begins at start value"""
+    self.start = start
+    self.currentCount = start
+
+def generate(self):
+    """Returns count, then increments by 1"""
+    self.currentCount += 1
+    return self.currentCount - 1
+
+def reset(self):
+    """Resets count to inital value"""
+    self.currentCount = self.start.val()
+    print(self.start)
+
+serial = SerialGenerator(start=100)
