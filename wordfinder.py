cd7fe938e6264d27f37385b8823186fe92319060
Joshua <jhellst@gmail.com>
2023-08-22 16:10:39 -0700

    Added functionality for initiating words_list from a text file and returning count

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,24 @@
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+
+
+    def __init__(self, filepath):
+        """Create a word finder instance by reading a provided file of words."""
+
+        (self.words_list, count) = self.construct_wordslist_and_count(filepath)
+
+        print(f"{count} words read")
+
+    # @classmethod
+    def construct_wordslist_and_count(self, filepath):
+        words_list = set()
+
+        with open(filepath, "r") as file:
+            num_words = 0
+            for line in file:
+                words_list.add("".join(line.split("\n")))
+                num_words += 1
+
+        return (words_list, num_words)
+
+
