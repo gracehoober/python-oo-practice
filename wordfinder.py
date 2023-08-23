663df84e760351c0dc478efd80d78a7768e1cc02
Joshua <jhellst@gmail.com>
2023-08-22 17:22:14 -0700

    Added full functionality for random() and new class SpecialWordFinder

--- wordfinder.py
+++ wordfinder.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
 
@@ -5,34 +7,61 @@ class WordFinder:
     def __init__(self, filepath):
         """Create a word finder instance by reading a provided file of words."""
 
-        (self.words_list, count) = self.construct_wordslist_and_count(filepath)
+        self.words_list = self.construct_wordslist_and_count(filepath)
 
-        print(f"{count} words read")
+        print(f"{len(self.words_list)} words read")
 
     # @classmethod
-    def construct_wordslist_and_count(self, filepath):
+    def construct_wordslist_and_count(self, filepath): # TODO: Change method name to be concise
         """Reads a file of words returns a list of words and count of words"""
-        words_list = set()
+        # words_list = set()
 
-        with open(filepath, "r") as file:
-            num_words = 0
-            for line in file:
-                words_list.add("".join(line.split("\n")))
-                num_words += 1
+        file = open(filepath, "r")
+        words_list = [line.strip() for line in file]
 
-        return (words_list, num_words)
+        # with open(filepath, "r") as file:
+        #     for line in file:
+        #         # words_list.add("".join(line.split("\n")))
+        #         words_list.append("".join(line.split("\n"))) # Note: strip() is another option that avoids having to join, specifically removes whitespace
+
+        return words_list
 
 
     def random(self):
         """Returns a random word from the words list"""
-        removed = self.words_list.pop()
-        self.words_list.add(removed)
-        return removed
+        # removed = self.words_list.pop()
+        # self.words_list.add(removed)
+
+        # Note: choice() allows to select random item from a sequence
+        randomIndex = randint(0, len(self.words_list) - 1)
+        return self.words_list[randomIndex]
+
+
+        # return removed
 
 class SpecialWordFinder(WordFinder):
+    """Special Word Finder: finds random words from a dictionary, but excludes comments (#) and blank lines."""
 
     def __init__(self, filepath):
+        """Create a special word finder instance by reading a provided file of words."""
         super().__init__(filepath)
+        self.remove_invalid_words()
+
+
+    def remove_invalid_words(self):
+        """
+        Process the words list further to remove blank spaces and comments starting with '#'.
+        Called when instance of class is created.
+        """
+
+        # TODO: List comprehension here to be more pythonic
+        new_words_list = []
 
         for word in self.words_list:
-            if word[0] == "#" or word ==
+            if not (not word or word[0] == "#"):
+                new_words_list.append(word)
+
+
+        self.words_list = new_words_list
+
+        #TODO: REPR for both classes
